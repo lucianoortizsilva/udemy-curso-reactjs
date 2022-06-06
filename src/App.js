c2ca60d608c6aa25104b70e23dde8d5c25fc7bef
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-06-06 20:34:03 -0300

    Hello World React JS

---
+++ src/App.js
@@ -0,0 +1,11 @@
+/** 
+ * 'export' para ser utilizado em outros arquivos
+ */
+export default function App(){
+  return(
+    <div>
+      <h1>Bem vindo ao sistema</h1>
+      <h2>lucianoortizsilva@gmail.com</h2>
+    </div>
+  )
+}
