154fdbbab80f68ebf5afe57323ee414806a75e5f
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-06-07 20:57:15 -0300

    Add Formularios Cont...

--- src/App.js
+++ src/App.js
@@ -1,55 +1,53 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 
 class App extends Component{
-  
-  constructor(props){
-    super(props);
-    this.state = {
-      email: 'luciano.ortiz@neogrid',
-      senha: '123456',
-      sexo: 'Masculino'
+
+    constructor(props){
+        super(props);
+        this.state = { 
+            email: '',
+            senha: '',
+            sexo: 'masculino'
+         }
+
+         this.trocaEmail = this.trocaEmail.bind(this);
+         this.trocaSexo = this.trocaSexo.bind(this);
     }
-    this.trocarEmail = this.trocarEmail.bind(this);
-    this.trocaSexo = this.trocaSexo.bind(this);
-  }
 
-  trocarEmail(event){
-    let valorDigitado = event.target.value
-    this.setState({email: valorDigitado})
-  }
+    trocaEmail(e){
+        let valorDigitado = e.target.value;
+        this.setState({email: valorDigitado});
+    }
 
-  trocaSexo(event){
-    let sexoDigitado = event.target.value
-    this.setState({sexo: sexoDigitado})
-  }
+    trocaSexo(e){
+        let valorDigitado = e.target.value
+        this.setState({sexo: valorDigitado});
+    }
 
-  render(){
-    return(
-      <div>
-        <h2>Login</h2>
-        Email:
-        <input type="email" 
-               name="email" 
-               value={this.state.email} 
-               onChange={this.trocarEmail}/><br/>
-        Senha:        
-        <input type="password" 
-               name="senha" 
-               value={this.state.senha}
-               onChange={(event)=> this.setState({senha: event.target.value})}/><br/>
+    render(){
+        return(
+            <div>
+                <h2>Login</h2>
+                Email:
+                <input type="email" name="email" value={this.state.email} 
+                       onChange={this.trocaEmail} /> <br/>
+                Senha:
+                <input type="text" name="senha" value={this.state.senha}
+                       onChange={(e) => this.setState({senha: e.target.value})} /><br/>
 
-       Sexo:
-       <select name="sexo" value={this.state.sexo} onChange={this.trocaSexo}>
-         <option value="Masculino">Masculino</option>
-         <option value="Feminino">Feminino</option>
-       </select>
-      
-      <br/>
-        <h3>Sexo selecionado:</h3>
-        <span>{this.state.sexo}</span>
-      </div>
-    );
-  }
+                Sexo:
+                <select name="sexo" value={this.state.sexo} onChange={this.trocaSexo}>
+                    <option value="masculino">Masculino</option>
+                    <option value="feminino">Feminino</option>
+                </select>
+
+                <div>
+                    <h3>{this.state.email}</h3>
+                    <h3>{this.state.senha}</h3>
+                </div>
+            </div>         
+        );
+    }
 }
 
 export default App;
