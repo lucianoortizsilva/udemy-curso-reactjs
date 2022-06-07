5c621eb493e999946c0b9a8f40178d7a9a51bcfb
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-06-07 20:39:38 -0300

    Praticando formulários

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React, {Component} from 'react'
-import Membro from './components/Membro';
 
 class App extends Component{
   
+  constructor(props){
+    super(props);
+    this.state = {
+      email: 'luciano.ortiz@neogrid',
+      senha: '123456',
+      sexo: 'Masculino'
+    }
+    this.trocarEmail = this.trocarEmail.bind(this);
+    this.trocaSexo = this.trocaSexo.bind(this);
+  }
+
+  trocarEmail(event){
+    let valorDigitado = event.target.value
+    this.setState({email: valorDigitado})
+  }
+
+  trocaSexo(event){
+    let sexoDigitado = event.target.value
+    this.setState({sexo: sexoDigitado})
+  }
+
   render(){
     return(
       <div>
-        <Membro nome="Visitante"/>
+        <h2>Login</h2>
+        Email:
+        <input type="email" 
+               name="email" 
+               value={this.state.email} 
+               onChange={this.trocarEmail}/><br/>
+        Senha:        
+        <input type="password" 
+               name="senha" 
+               value={this.state.senha}
+               onChange={(event)=> this.setState({senha: event.target.value})}/><br/>
+
+       Sexo:
+       <select name="sexo" value={this.state.sexo} onChange={this.trocaSexo}>
+         <option value="Masculino">Masculino</option>
+         <option value="Feminino">Feminino</option>
+       </select>
+      
+      <br/>
+        <h3>Sexo selecionado:</h3>
+        <span>{this.state.sexo}</span>
       </div>
     );
   }
