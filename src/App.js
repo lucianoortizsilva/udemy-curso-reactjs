e2503e22ab1fb8f1146ed12228a4efc65afd863c
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-06-08 13:50:59 -0300

    Aprendendo useState do kooks

--- src/App.js
+++ src/App.js
@@ -1,66 +1,29 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './style.css'
 
-// Importando a imagem do cronometro
-import cronometro from './assets/cronometro.png'; 
+function App(){
 
-class App extends Component{
+  const [tarefas, setTarefas] = useState(['Pagar conta Luz','Estudar isso']);
+  const [nome, setNome] = useState('');
 
-  constructor(props){
-    super(props);
-    this.state = {
-      numero: 0,
-      botao: 'VAI'
-    };
-    this.timer = null;
-    this.vai = this.vai.bind(this);
-    this.limpar = this.limpar.bind(this);
+  return(
+    <div>
+      <ul>
+        {
+          tarefas.map(tarefa => (
+            <li key={tarefa}>{tarefa}</li>
+          ))
+        }
+      </ul>
+      <input type={Text} value={nome} onChange={(e)=> setNome(e.target.value)}/>
+      <button type='button' onClick={add}>Adicionar</button>
+    </div>
+  );
+
+  function add(){
+    setTarefas([...tarefas, nome])
+    setNome('');
   }
 
-  vai(){
-    let state = this.state;
-
-    if(this.timer !== null){
-      clearInterval(this.timer);
-      this.timer = null;
-      state.botao = 'VAI';
-    }else{
-      this.timer = setInterval(()=>{
-        let state = this.state;
-        state.numero += 0.1;
-        this.setState(state);
-      },100);
-      state.botao = 'PAUSAR';
-    }
-
-    this.setState(state);
-  }
-
-  limpar(){
-    if(this.timer !== null){
-      clearInterval(this.timer);
-      this.timer = null;
-    }
-
-    let state = this.state;
-    state.numero = 0;
-    state.botao = 'VAI';
-    this.setState(state);
-
-  }
-
-  render(){
-    return(
-      <div className="container">
-        <img src={cronometro} className="img" />
-        <a className="timer">{this.state.numero.toFixed(1)}</a>
-        <div className="areaBtn">
-          <a className="botao" onClick={this.vai}>{this.state.botao}</a>
-          <a className="botao" onClick={this.limpar}>LIMPAR</a>
-        </div>
-      </div>
-    );
-  }
 }
-
 export default App;
