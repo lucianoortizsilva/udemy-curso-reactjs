a23eb10f41c79332f64dff3e1b14cc345c96450f
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-06-09 13:54:56 -0300

    Add rotas com parâmetros

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,6 +8,10 @@ function Home() {
 
       <Link to="/sobre">Sobre</Link> <br/>
       <Link to="/contato">Contato</Link>
+
+      <hr/>
+
+      <Link to="/produto/12345">Acessar Produto 12345</Link>
     </div>
   );
 }
