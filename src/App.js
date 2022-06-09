c2447c30cdd3e0b15efcaa7c4d9f9a17e2347f09
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-06-09 08:44:33 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-import React, {useState} from 'react';
-import './style.css'
+import React, { useState, useEffect } from 'react';
 
-function App(){
 
-  const [tarefas, setTarefas] = useState(['Pagar conta Luz','Estudar isso']);
-  const [nome, setNome] = useState('');
+function App() {
 
-  return(
-    <div>
-      <ul>
-        {
-          tarefas.map(tarefa => (
-            <li key={tarefa}>{tarefa}</li>
-          ))
-        }
-      </ul>
-      <input type={Text} value={nome} onChange={(e)=> setNome(e.target.value)}/>
-      <button type='button' onClick={add}>Adicionar</button>
-    </div>
-  );
+  const [tarefas, setTarefas] = useState([]);
+  const [input, setInput] = useState('');
 
-  function add(){
-    setTarefas([...tarefas, nome])
-    setNome('');
+  /**
+  *  Chamado após montar a tela
+  */
+  useEffect(()=>{
+    const tarefasStorage = localStorage.getItem('tarefas');
+
+    if(tarefasStorage){
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+
+  }, []);
+
+
+  useEffect(()=> {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }, [tarefas]);
+
+
+  function handleAdd(){
+    setTarefas([...tarefas, input])
+    setInput('');
   }
 
+  return (
+    <div>
+
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+
+      <input type="text" value={input} onChange={e => setInput(e.target.value)}/>    
+      <button type="button" onClick={handleAdd}>Adicionar</button>
+
+    </div>
+  );
 }
+
 export default App;
