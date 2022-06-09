c9099f64bc96cff8323de13296b4485f9e7a7452
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-06-09 15:10:49 -0300

    [Filmes] - Listando filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState} from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 // URL DA API: /movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
 
@@ -18,7 +20,8 @@ function Home(){
         }
       })
 
-      console.log(response.data.results);
+      //console.log(response.data.results.slice(0, 10));
+      setFilmes(response.data.results.slice(0, 10))
 
     }
 
@@ -27,8 +30,18 @@ function Home(){
   }, [])
 
   return(
-    <div>
-      <h1>BEM VINDO A HOME</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
     </div>
   )
 }
