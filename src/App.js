d507fedb60561248b9352b2802448d8ff7cd9405
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-06-13 10:14:33 -0300

    FIREBASE Config + Add in collection

--- src/App.js
+++ src/App.js
@@ -1,13 +1,97 @@
-import RoutesApp from './routes';
-import { ToastContainer } from 'react-toastify';
+import firebase from './firebaseConnection';
+import {useState} from 'react';
+import './style.css';
 
-import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  async function handleAdd(){
+    
+    await firebase.firestore().collection('posts')
+    .add({
+      titulo: titulo,
+      autor: autor,
+    })
+    .then(()=>{
+      console.log('DADOS CADASTRADO COM SUCESSO!');
+      setTitulo('');
+      setAutor('');
+    })
+    .catch((error)=>{
+      console.log('GEROU ALGUM ERRO: ' + error);
+    })
+
+  }
+
+
+  async function buscaPost(){
+    //  await firebase.firestore().collection('posts')
+    //  .doc('123')
+    //  .get()
+    //  .then((snapshot)=>{
+
+    //   setTitulo(snapshot.data().titulo);
+    //   setAutor(snapshot.data().autor);
+
+    //  })
+    //  .catch(()=>{
+    //    console.log('DEU ALGUM ERRO')
+    //  })
+
+    await firebase.firestore().collection('posts')
+    .get()
+    .then((snapshot)=>{
+      let lista = [];
+
+      snapshot.forEach((doc)=>{
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor
+        })
+      })
+
+      setPosts(lista);
+
+    })
+    .catch(()=>{
+      console.log('DEU ALGUM ERRO!');
+    })
+
+
+  }
+
   return (
-    <div className="App">
-      <ToastContainer autoClose={3000}/>
-      <RoutesApp/>
+    <div>
+      <h1>ReactJS + Firebase :)</h1> <br/>
+
+    <div className="container">
+
+    <label>Titulo: </label>
+    <textarea type="text" value={titulo} onChange={ (e) => setTitulo(e.target.value)} />
+
+    <label>Autor: </label>
+    <input type="text" value={autor} onChange={ (e) => setAutor(e.target.value) }  />
+
+    <button onClick={ handleAdd }>Cadastrar</button>
+    <button onClick={ buscaPost }>Buscar Post</button> <br/>
+
+    <ul>
+      {posts.map((post)=>{
+        return(
+          <li key={post.id} >
+            <span>Titulo: {post.titulo} </span> <br/>
+            <span>Autor: {post.autor} </span> <br/> <br/>
+          </li>
+        )
+      })}
+    </ul>
+
+    </div>
+
     </div>
   );
 }
